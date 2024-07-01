a89eeb15ca3a15def2b106d5bbd4bf2f635fa2d6
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-07-01 15:31:20 -0300

    add findAll User

---
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.devsuperior.workshopmongo.services;
+
+import com.devsuperior.workshopmongo.models.dto.UserDTO;
+import com.devsuperior.workshopmongo.models.entities.User;
+import com.devsuperior.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> findAll() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(UserDTO::new).toList();
+    }
+}
